71b9d22edbf09b8e4373fac963857797fcef3a92
Polastrine <diogo.dsilva@sptech.school>
2024-05-01 22:05:40 -0300

    Alterações no login

--- public/js/login.js
+++ public/js/login.js
@@ -54,18 +54,23 @@ function mudarTela1() {
     const inputSenha3 = document.getElementById("inputSenha3");
 
 
-    boxCinza.style.transform = "translateX(100%)"
-    boxRosa.style.transform = "translateX(-100%)"
-    boxCinza.style.borderRadius = "0 50px 50px 0";
-    boxRosa.style.borderRadius = "50px 0 0 50px";
-    boxRosaContent1.style.display = "none";
-    boxRosaContent2.style.display = "flex";
-    boxCinzaContent1.style.display = "none";
-    boxCinzaContent2.style.display = "block";
-    inputNome1.value = '';
-    inputEmail2.value = '';
-    inputSenha2.value = '';
-    inputSenha3.value = '';
+    
+        boxCinza.style.transform = "translateX(100%)";
+        boxRosa.style.transform = "translateX(-100%)";
+        boxCinza.style.borderRadius = "0 50px 50px 0";
+        boxRosa.style.borderRadius = "50px 0 0 50px";
+        inputNome1.value = '';
+        inputEmail2.value = '';
+        inputSenha2.value = '';
+        inputSenha3.value = '';
+
+
+    setTimeout(function() {    
+        boxRosaContent1.style.display = "none";
+        boxRosaContent2.style.display = "flex";
+        boxCinzaContent1.style.display = "none";
+        boxCinzaContent2.style.display = "block";
+    }, 300); // Tempo em milissegundos (300ms = 0.3 segundos)
 }
 
 function mudarTela2() {
@@ -78,15 +83,19 @@ function mudarTela2() {
     const inputEmail1 = document.getElementById("inputEmail1");
     const inputSenha1 = document.getElementById("inputSenha1");
 
-    boxCinza.style.transform = "translateX(-0%)"
-    boxRosa.style.transform = "translateX(0%)"
-    boxCinza.style.borderRadius = "50px 0 0 50px";
-    boxRosa.style.borderRadius = "0 50px 50px 0";
-    boxRosaContent1.style.display = "flex";
-    boxRosaContent2.style.display = "none";
-    boxCinzaContent1.style.display = "block";
-    boxCinzaContent2.style.display = "none";
-    inputEmail1.value = '';
-    inputSenha1.value = '';
+    
+        boxCinza.style.transform = "translateX(-0%)";
+        boxRosa.style.transform = "translateX(0%)";
+        boxCinza.style.borderRadius = "50px 0 0 50px";
+        boxRosa.style.borderRadius = "0 50px 50px 0";
+        inputEmail1.value = '';
+        inputSenha1.value = '';
+
+    setTimeout(function() {    
+        boxCinzaContent1.style.display = "block";
+        boxCinzaContent2.style.display = "none";
+        boxRosaContent1.style.display = "flex";
+        boxRosaContent2.style.display = "none";
+    }, 300); // Tempo em milissegundos (300ms = 0.3 segundos)
 }
   
