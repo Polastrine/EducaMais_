fca3d6fc5e301a3b99d4cd7243685f567b68955a
Polastrine <diogo.dsilva@sptech.school>
2024-05-19 01:32:43 -0300

    Finalização da primeira tela da plataforma e estruturação das demais

---
+++ public/js/plataformaHome.js
@@ -0,0 +1,19 @@
+function acessarHome(){
+    window.location.href='./plataformaHome.html'
+}
+
+function acessarForum(){
+    window.location.href='./plataformaForum.html'
+}
+
+function acessarQuiz(){
+    window.location.href='./plataformaQuiz.html'
+}
+
+function acessarEstatisticas(){
+    window.location.href='./plataformaEstatisticas.html'
+}
+
+function voltarHome(){
+    window.location.href='./index.html'
+}
